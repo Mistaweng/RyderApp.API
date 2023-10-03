897f51bb9d42e937a2c5616089fddd387b80d138
africatcf <dev@africatcf.org>
2023-10-03 17:07:35 +0100

    Added Enums

---
+++ RyderApp.Domain/Enums/CardStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyderApp.Domain.Enums
+{
+	public enum CardStatus
+	{
+		Active = 1,
+		Expired = 2,
+		InActive = 3,
+
+	}
+}
---
+++ RyderApp.Domain/Enums/PaymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RyderApp.Domain.Enums
+{
+	public enum PaymentStatus
+	{
+		Initiated = 1,
+		Pending = 2,
+		Failed = 3,
+		Successful = 4
+	}
+}
